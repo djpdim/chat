1c85219fdb1e2de7eb86b1217e2f5ed76cc32949
Dimos <dimospagkas@yahoo.gr>
2023-03-28 09:53:25 -0400

    new project Chat Box with chatGPT AI Capabilities

---
+++ src/components/chat/index.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/customHeader"
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced"
+
+const Chat = () => {
+    const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID, "testuser", "1234")
+    return (
+        <div style={{ flexBasis: "100%" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow
+                {...chatProps}
+                style={{ height: "100vh" }}
+                renderChatHeader={chat => <Header chat={chat} />}
+            />
+        </div>
+    )
+}
+
+export default Chat
