9da30aeefe50a35bbe967f35d77944efae2d5aab
Dimos <dimospagkas@yahoo.gr>
2023-03-29 15:50:27 -0400

    create routes and message form for chat

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -6,8 +6,11 @@ const router = express.Router()
 
 router.post("/text", async (req, res) => {
     try {
+        const { text, activeChatId } = req.body
     } catch (error) {
         console.log("error", error)
         res.status(500).json({ error: error.message })
     }
 })
+
+export default router
