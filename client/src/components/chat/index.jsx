902356861e8e5abcb67f2cf60c60abade2a5d73c
Dimos <dimospagkas@yahoo.gr>
2023-03-30 09:12:18 -0400

    create aiCode aiAssist

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,4 +1,7 @@
 import Header from "@/components/customHeader"
+import Ai from "@/components/customMessageForms/Ai"
+import AiAssist from "@/components/customMessageForms/AiAssist"
+import AiCode from "@/components/customMessageForms/AiCode"
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced"
 
@@ -12,6 +15,15 @@ const Chat = () => {
                 style={{ height: "100vh" }}
                 renderChatHeader={chat => <Header chat={chat} />}
                 renderChatMessageForm={props => {
+                    if (chatProps.chat?.title.startsWith("AiChat_")) {
+                        return <Ai props={props} activeChat={chatProps.chat} />
+                    }
+                    if (chatProps.chat?.title.startsWith("AiCode_")) {
+                        return <AiCode props={props} activeChat={chatProps.chat} />
+                    }
+                    if (chatProps.chat?.title.startsWith("AiAssist_")) {
+                        return <AiAssist props={props} activeChat={chatProps.chat} />
+                    }
                     return <StandardMessageForm props={props} activeChat={chatProps.chat} />
                 }}
             />
